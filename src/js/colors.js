e9c652dda33328ef34f3d4eac77489c4474477bb
John Patrick C. Perez <patrickperez527@gmail.com>
2022-03-03 17:57:38 +0800

    initial commit

---
+++ src/js/colors.js
@@ -0,0 +1,40 @@
+let theme = localStorage.getItem('theme');
+
+if (theme == null) {
+	setTheme('dark');
+} else {
+	setTheme(theme);
+}
+
+let themeDots = document.querySelectorAll('.theme-dot');
+
+for (let i = 0; themeDots.length > i; i++){
+	themeDots[i].addEventListener('click', function(){
+		let mode = this.dataset.mode;
+		setTheme(mode);
+	})
+}
+
+function setTheme(mode){
+	if(mode == 'dark'){
+		document.getElementById('theme-style').href = 'src/css/styles.css';
+	}
+
+	if(mode == 'blue'){
+		document.getElementById('theme-style').href = 'src/css/blue.css';
+	}
+
+	if(mode == 'green'){
+		document.getElementById('theme-style').href = 'src/css/green.css';
+	}
+
+	if(mode == 'purple'){
+		document.getElementById('theme-style').href = 'src/css/purple.css';
+	}
+
+	if(mode == 'light'){
+		document.getElementById('theme-style').href = 'src/css/light.css';
+	}
+
+	localStorage.setItem('theme', mode);
+}
---
+++ src/js/script.js
@@ -0,0 +1,124 @@
+const arrowUpButton = document.getElementById('arrow-up-button');
+
+window.onscroll = function() {
+    scrollFunction()
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    arrowUpButton.style.display = "block";
+  } else {
+    arrowUpButton.style.display = "none";
+  }
+};
+
+arrowUpButton.addEventListener('click', () => {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+});
+
+const heroText = document.getElementById('hero-text');
+const heroTextContent = 'Aspiring Full Stack Developer';
+let count = 1;
+let speed = 100;
+
+
+writeText();
+
+function writeText(){
+    heroText.innerHTML = heroTextContent.slice(0, count) + ' <i class="fa fa-code" aria-hidden="true"></i>';
+    count++;
+
+    if (count > heroTextContent.length) {
+        count = 1;
+    }
+    setTimeout(writeText, speed);
+}
+
+const myForm = document.getElementById('form');
+const nameInput = document.getElementById('name');
+const emailInput = document.getElementById('email');
+const companyInput = document.getElementById('company');
+const messageInput = document.getElementById('message');
+const formErrorMessage = document.getElementById('error');
+
+myForm.addEventListener('submit', e => {
+    e.preventDefault();
+    if (nameInput.value === '' || emailInput.value === '' || companyInput.value === '' || messageInput.value === '') {
+        formErrorMessage.innerText = 'You must fill in all of the fields.';
+        formErrorMessage.style.background = '#dc3545';
+        formErrorMessage.style.marginBottom = '10px';
+        hideMessage();
+    } else if (!nameInput.value.match(/^[{3}a-z ,.'-]+$/i)) {
+        formErrorMessage.innerText = 'Please enter a valid name.';
+        formErrorMessage.style.background = '#dc3545';
+        formErrorMessage.style.marginBottom = '10px';
+        formErrorMessage.style.width = '50%';
+        hideMessage();
+    } else if (!emailInput.value.match(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)) {
+        formErrorMessage.innerText = 'Please enter a valid email address.';
+        formErrorMessage.style.background = '#dc3545';
+        formErrorMessage.style.marginBottom = '10px';
+        formErrorMessage.style.width = '65%';
+        hideMessage();
+    } else if (!companyInput.value.match(/^[{3}a-z ,.'-]+$/i)) {
+        formErrorMessage.innerText = 'Company name should only contain letters.';
+        formErrorMessage.style.background = '#dc3545';
+        formErrorMessage.style.marginBottom = '10px';
+        formErrorMessage.style.width = '80%';
+        hideMessage();
+    } else {
+        myForm.submit();
+    }   
+})
+
+function hideMessage(){
+    setTimeout(() => {
+        formErrorMessage.innerText = '';
+        formErrorMessage.style.background = 'transparent';
+        formErrorMessage.style.marginBottom = 0;
+    }, 3000)
+}
+
+// form button interaction
+const formButton = document.querySelector('.submit');
+formButton.addEventListener('mouseover', () => {
+    if (nameInput.value === '' || emailInput.value === '' || companyInput.value === '' || messageInput.value === '') {
+        formButton.style.cursor = 'not-allowed';
+    } else {
+        formButton.style.cursor = 'pointer';
+    }
+})
+
+// form input interaction
+nameInput.addEventListener('mouseover', () => {
+    nameInput.placeholder = 'Enter your name';
+})
+
+nameInput.addEventListener('mouseleave', () => {
+    nameInput.placeholder = 'Name';
+})
+
+emailInput.addEventListener('mouseover', () => {
+    emailInput.placeholder = 'Enter your email address';
+})
+
+emailInput.addEventListener('mouseleave', () => {
+    emailInput.placeholder = 'Email';
+})
+
+companyInput.addEventListener('mouseover', () => {
+    companyInput.placeholder = 'Enter your company or location';
+})
+
+companyInput.addEventListener('mouseleave', () => {
+    companyInput.placeholder = 'Company';
+})
+
+messageInput.addEventListener('mouseover', () => {
+    messageInput.placeholder = 'Write your message';
+})
+
+messageInput.addEventListener('mouseleave', () => {
+    messageInput.placeholder = 'Message';
+})
